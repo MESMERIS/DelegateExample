31c691bdbb40b7d1ce1382e637c7a3e1ecc7adf6
anand80 <anandratnam@live.com.my>
2016-12-19 15:11:28 +0800

    Add project files.

---
+++ DelegateExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public delegate void HelloFunctionDelegate(string Message);
+class program
+{
+    public static void Main()
+    {
+        HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
+        Hello("Hello From Delegate");
+    }
+    public static void Hello(string strMessage)
+    {
+        Console.WriteLine(strMessage);
+        Console.ReadLine();
+    }
+}
